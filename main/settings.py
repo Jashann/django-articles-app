401c5064a3da9a4d77ac66965ff045cfeba32192
Jashann <jashangill3592@gmail.com>
2021-05-30 21:30:19 +0530

    Reset password using app password

--- main/settings.py
+++ main/settings.py
@@ -151,17 +151,13 @@ LOGIN_REDIRECT_URL = "/articles"
 REDIRECT_AUTHENTICATED_USER = True
 
 # PASSWORD RESET EMAIL
-# config/settings.py
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anything3592@gmail.com'
-EMAIL_HOST_PASSWORD = 'nopasswordpublic'
-
-
-
+EMAIL_HOST_PASSWORD = 'yszfhiivmvaqdnbk'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Thoughts Share - Password Reset <noreply@anything3592.com>'
 
 
 
